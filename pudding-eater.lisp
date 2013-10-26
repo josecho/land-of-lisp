c3606164bb1874c09998b663cc34a39f64599b91
plausibility <chris@gibsonsec.org>
2013-10-26 21:02:06 +1100

    WHY YOU EAT MY PUDDING STRANGER?

--- pudding-eater.lisp
+++ pudding-eater.lisp
@@ -1,14 +1,21 @@
 (defvar *arch-enemy* nil)
 
+;;(defun pudding-eater (person)
+;;    (cond ((eq person 'henry) (setf *arch-enemy* 'stupid-lisp-alien)
+;;                               '(curse you lisp alien - you ate my pudding))
+;;          ((eq person 'johnny) (setf *arch-enemy* 'useless-old-johnny)
+;;                               '(I hope you choked on my pudding johnny))
+;;          (t                   '(why you eat my pudding stranger ?))))
+
 (defun pudding-eater (person)
-    (cond ((eq person 'henry) (setf *arch-enemy* 'stupid-lisp-alien)
-                               '(curse you lisp alien - you ate my pudding))
-          ((eq person 'johnny) (setf *arch-enemy* 'useless-old-johnny)
-                               '(I hope you choked on my pudding johnny))
-          (t                   '(why you eat my pudding stranger ?))))
+    (case person
+        ((henry)   (setf *arch-enemy* 'stupid-lisp-alien)
+                   '(curse you lisp alien - you ate my pudding))
+        ((johnny)  (setf *arch-enemy* 'useless-old-johnny)
+                   '(I hope you choked on my pudding johnny))
+        (otherwise '(why you eat my pudding stranger?))))
 
-(pudding-eater 'johnny)
+(print (pudding-eater 'johnny))
 (print *arch-enemy*)
 
-(pudding-eater 'george-clooney)
-(print *arch-enemy*)
+(print (pudding-eater 'george-clooney))
