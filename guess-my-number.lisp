32120e5ae451ebdbdc7e1fb7f01901aeef4b3e0b
plausibility <chris@gibsonsec.org>
2013-10-26 20:56:19 +1100

    tabs -> spaces, this ain't python

--- guess-my-number.lisp
+++ guess-my-number.lisp
@@ -2,17 +2,17 @@
 (defparameter *big* 100)
 
 (defun guess-my-number ()
-	(ash (+ *small* *big*) -1))
+    (ash (+ *small* *big*) -1))
 
 (defun smaller ()
-	(setf *big* (1- (guess-my-number)))
-	(guess-my-number))
+    (setf *big* (1- (guess-my-number)))
+    (guess-my-number))
 
 (defun bigger ()
-	(setf *small* (1+ (guess-my-number)))
-	(guess-my-number))
+    (setf *small* (1+ (guess-my-number)))
+    (guess-my-number))
 
 (defun start-over ()
-	(defparameter *small* 1)
-	(defparameter *big* 100)
-	(guess-my-number))
+    (defparameter *small* 1)
+    (defparameter *big* 100)
+    (guess-my-number))
